70bc1d892696b34ad1ca4cf0c59dfa0b0e11add0
Francesco Casalegno <francesco.casalegno@epfl.ch>
2016-11-28 18:16:38 +0100

    Anisotropic adaptivity based on Hessian recovery working! (almost: see corner)

--- test/aniso_ma_test.cc
+++ test/aniso_ma_test.cc
@@ -1,5 +1,6 @@
 #include "ma.h"
 #include <apf.h>
+#include <apfNumbering.h>
 #include <gmi_mesh.h>
 #include <apfMDS.h>
 #include <apfShape.h>
@@ -10,57 +11,78 @@
 #include <cstdlib>
 #include <algorithm>
 
+#include <spr.h>
+
 #include "mthQR.h"
 
 class AnIso : public ma::AnisotropicFunction
 {
 public:
-    AnIso(ma::Mesh* m)
+    
+    /** \brief C-tor for mesh adaptor
+     * \param m mesh
+     * \param hess_fld recovered Hessian field
+     */
+    AnIso(apf::Mesh2* m, apf::Field* hess_fld) :
+        mesh(m),
+        hess(hess_fld)
+    { }
+    
+    /** \brief get prescribed rotation and deformation at a given mesh vertex 
+     * \param v mesh vertex
+     * \param R rotation matrix
+     * \param S deformation vector
+     */
+    virtual void getValue(apf::MeshEntity* v, apf::Matrix3x3& R, apf::Vector3& S)
     {
-        mesh = m;
-        average = ma::getAverageEdgeLength(m);
-        ma::getBoundingBox(m,lower,upper);
-    }
-    virtual void getValue(ma::Entity* v, ma::Matrix& R, ma::Vector& H)
-    {
-        ma::Vector p = ma::getPosition(mesh,v);
-        double x = p[0], y = p[1], z = p[2];
-        double Pe = 1.5;
+        // Get exact Hessian at vertex v
+        //apf::Vector3 p = ma::getPosition(mesh,v);
+        //double x = p[0], y = p[1], z = p[2];
+        //double Pe = 1.5;
+        //double uxx = 9*Pe*Pe*std::exp(Pe*(3*x-y));
+        //double uxy =-3*Pe*Pe*std::exp(Pe*(3*x-y));
+        //double uyy =  Pe*Pe*std::exp(Pe*(3*x-y));
+        //double uxz = 0.;
+        //double uyz = 0.;
+        //double uzz = 0.;
+        //apf::Matrix3x3 H(uxx, uxy, uxz,
+        //        uxy, uyy, uyz,
+        //        uxz, uyz, uzz);
 
-        double uxx = 9*Pe*Pe* exp(Pe*(3*x-y));
-        double uxy =-3*Pe*Pe* exp(Pe*(3*x-y));
-        double uyy =  Pe*Pe* exp(Pe*(3*x-y));
-        ma::Matrix r(uxx, uxy, 0.,
-                uxy, uyy, 0.,
-                0., 0., 1.);
+        // Get Hessian tensor at vertex v
+        apf::Matrix3x3 H;
+        getMatrix(hess, v, 0, H);
 
+        // Decomposition of Hessian: A = Q'*Lambda*Q
         mth::Matrix<double,3,3> A, Q, Lambda;
-        for (int i=0; i<3; ++i) for (int j=0; j<3; ++j) A(i,j) = r[i][j];
-
-        // Decomposition: A = Q'*Lambda*Q
+        A = *reinterpret_cast<mth::Matrix<double,3,3>* >(&H);
         mth::eigenQR(A, Lambda, Q, 100);
 
         // Rotation Matrix
-        for (int i=0; i<3; ++i) 
-            for (int j=0; j<3; ++j) 
-                R[i][j] = Q(i,j);
+        R = *reinterpret_cast<ma::Matrix*>(&Q);
+        
         // Deformation Matrix
-        double epsilon    = 0.1;
+        double epsilon    = 10;
         double dim        = 2;
         double c_d        = 1./2. * std::pow(dim/(dim+1),2);
         double h_min      = 0.001;
         double h_max      = 0.5;
-        for (int i=0; i<3; ++i) 
-            H[i] = std::min( std::max(10./(1.e-22 + std::abs(Lambda(i,i))), h_min), h_max ); 
+        for (int i=0; i<3; ++i)  
+            S[i] = std::min( std::max(epsilon/(1.e-22 + std::abs(Lambda(i,i))), h_min), h_max ); 
 
     }
 private:
-    ma::Mesh* mesh;
-    double average;
-    ma::Vector lower;
-    ma::Vector upper;
+    apf::Mesh2* mesh;
+    apf::Field* hess;
 };
 
+
+double fun(apf::Vector3 p) {
+    double x=p[0], y=p[1], z=p[2];
+    double Pe = 1.5;
+    return std::exp(Pe*(3*x-y));
+    //return std::pow(x,2)-x;
+}
 
 
 int main(int argc, char** argv)
@@ -71,10 +93,41 @@ int main(int argc, char** argv)
     MPI_Init(&argc,&argv);
     PCU_Comm_Init();
     gmi_register_mesh();
-    ma::Mesh* m = apf::loadMdsMesh(modelFile,meshFile);
+    apf::Mesh2* m = apf::loadMdsMesh(modelFile,meshFile);
     m->verify();
+    
+    
+    // I. Fields
+    apf::Field* fld = apf::createLagrangeField(m, "v", apf::SCALAR, 1);
+    apf::zeroField(fld); 
+    apf::GlobalNumbering*   owned = makeGlobal(apf::numberOwnedNodes(m, "owned", apf::getShape(fld)));
+    apf::GlobalNumbering*  shared = makeGlobal(apf::numberOwnedNodes(m, "shared", apf::getShape(fld)));
+    apf::synchronize(shared);
+    apf::DynamicArray<apf::Node> nodes;
+    apf::getNodes(owned, nodes);
+    for (size_t i=0; i < nodes.getSize(); ++i) {
+        apf::Vector3 p;
+        m->getPoint(nodes[i].entity, nodes[i].node, p);
+        apf::setScalar(fld, nodes[i].entity, nodes[i].node, fun(p));
+    }
+    apf::synchronize(fld);
+    apf::Field* g = spr::getGradIPField(fld, "g", 1);
+    apf::synchronize(g);
+    apf::Field* gstar = spr::recoverField(g);
+    apf::synchronize(gstar);
+    apf::Field* H = spr::getGradIPField(gstar, "H", 1);
+    apf::synchronize(H);
+    apf::Field* Hstar = spr::recoverField(H);
+    apf::synchronize(Hstar);
+    apf::destroyField(g);
+    apf::destroyField(gstar);
+    apf::destroyField(H);
+
     apf::writeVtkFiles("aniso_before",m);
-    AnIso sf(m);
+
+
+    // II. Mesh Adaptation    
+    AnIso sf(m, Hstar);
     ma::Input* in = ma::configure(m, &sf);
     in->shouldRunPreZoltan = true;
     in->shouldRunMidParma = true;
